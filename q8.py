fd6d79a9960b7296f805d3399ff1de157e87335d
PranavReddyGorla <118622312+PranavReddyGorla@users.noreply.github.com>
2024-05-10 17:43:47 +0530

    Update q8.py

--- q8.py
+++ q8.py
@@ -1,4 +1,4 @@
 original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
-new_dict_1 = {k: ('old' if v > 40 else 'young')
-for (k, v) in original_dict.items()}
+new_dict_1 = {k: ('old' if v > 40 else 'young')#if the value is over 40 it assigns old otherwise young
+for (k, v) in original_dict.items()}#iterates through the original dict
 print(new_dict_1)
