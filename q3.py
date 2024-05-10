6f6709890302142c6eaa249acf0f77074f920353
PranavReddyGorla <118622312+PranavReddyGorla@users.noreply.github.com>
2024-05-10 17:42:24 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,3 +1,3 @@
 dict_1 = {'name': 'Harry', 'age': 27, 'location': 'Helsinki'}
 dict_2 = {'job': 'Architect'}
-print(dict_1 | dict_2)
+print(dict_1 | dict_2)#by using the merge operator we can combine two dictionaries
