4496072f67a4ce02de0d97dc890f29b3b3c55ca8
PranavReddyGorla <118622312+PranavReddyGorla@users.noreply.github.com>
2024-05-10 17:41:41 +0530

    Update q1.py

--- q1.py
+++ q1.py
@@ -5,5 +5,5 @@ recurrence_dict = {
 }
 print("Before popping: ", recurrence_dict)
 # Lets remove the last item
-recurrence_dict.popitem()
-print("After popping: ", recurrence_dict)
+recurrence_dict.popitem()#list.pop = it removes and returns the last element of the list
+print("After popping: ", recurrence_dict)#dict.pop =  removes elements from a dictionary by key.
