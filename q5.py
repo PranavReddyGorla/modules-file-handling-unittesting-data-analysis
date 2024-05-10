a22c9a9f7c9689f9dca544bbb39812d43ade4827
PranavReddyGorla <118622312+PranavReddyGorla@users.noreply.github.com>
2024-05-10 17:43:00 +0530

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,4 +1,4 @@
 l1=[2,4,6]
 l2=[-2,-4,-6]
-for i in zip(l1, l2):
- print(i)
+for i in zip(l1, l2):#for loop iterates over the two lists
+ print(i)#zip -  pairs the elements at the same index from both lists together
