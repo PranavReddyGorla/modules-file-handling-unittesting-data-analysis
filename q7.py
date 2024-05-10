db3fde3998003ae17cb5c4a58ac9ac314cce4ad3
PranavReddyGorla <118622312+PranavReddyGorla@users.noreply.github.com>
2024-05-10 17:43:22 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,3 +1,3 @@
 original_dict = {'jack': 38, 'michael': 48, 'guido': 57, 'john': 33}
-new_dict = {k: v for (k, v) in original_dict.items() if v % 2 != 0 if v < 40}
+new_dict = {k: v for (k, v) in original_dict.items() if v % 2 != 0 if v < 40}#the value should not be divisible by 2 and should be less than 40
 print(new_dict)
